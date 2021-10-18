31be5e0b9fbc3d3e2cd79d2766a999ec53cc0454
浅陌丿 <1520190873@qq.com>
2021-10-18 22:03:49 +0800

    部署

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const CracoLessPlugin = require("craco-less");
 const resolve = (dir) => path.resolve(__dirname, dir);
+console.log(process.env.NODE_ENV);
 module.exports = {
   webpack: {
+    publicPath: process.env.NODE_ENV === "development" ? "/" : "/react-music",
     alias: {
       "@": resolve("src"),
     },
